45c7116ce529d0ff2256ff594f0ef3ddb0942795
Brenenho <brenohenryck@gmail.com>
2023-05-15 15:09:37 -0300

    Componentizado

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,20 +1,65 @@
-import imagemforca from "./assets/forca0.png"
+import imagemforca0 from "./assets/forca0.png"
+import imagemforca1 from "./assets/forca1.png"
+import imagemforca2 from "./assets/forca2.png"
+import imagemforca3 from "./assets/forca3.png"
+import imagemforca4 from "./assets/forca4.png"
+import imagemforca5 from "./assets/forca5.png"
+import imagemforca6 from "./assets/forca6.png"
+import React from "react";
+
+
 import palavras from "./palavras.js";
 
-export default function Jogo(){
+export default function Jogo(props) {
+  const { palavra, setPalavra, setDisabled, disabledclasse, setDisabledclasse, letrasSelecionadas, setLetrasSelecionadas, letrasDisabled, setLetrasDisabled, erros, setErros, letraposta, setLetraposta } = props;
+  const imagensForca = [imagemforca0, imagemforca1, imagemforca2, imagemforca3, imagemforca4, imagemforca5, imagemforca6];
+  const [palavraEscolhida, setPalavraEscolhida,] = React.useState(false);
+  const { palavraerrada, setPalavraerrada } = props;
 
-    
-  
-    return (
-        <div className="forca">
-        <img className="imagemforca" src={imagemforca} alt="" />
-        <button className="botaoescolher">Escolher palavra</button>
+  function resetJogo() {
+    setPalavra([]);
+    setDisabled(true);
+    setLetrasSelecionadas([]);
+    setLetrasDisabled(true);
+    setPalavraEscolhida(false);
+    setErros(0)
+    setPalavraerrada("")
+  }
+
+  function escolherPalavra(event) {
+    if (palavraEscolhida) {
+      resetJogo();
+    } else {
+      setLetrasSelecionadas([]);
+      setLetrasDisabled(false);
+      setDisabled(false);
+
+
+      let palavraSortida = palavras[Math.floor(Math.random() * palavras.length)];
+      let array = palavraSortida.split('');
+
+      setPalavra(array);
+
+      setPalavraEscolhida(true);
+      console.log(palavraSortida)
+      console.log(array)
+      console.log(palavra)
+    }
+  }
+
+  return (
+    <div className="forca">
+      <img className="imagemforca" src={imagensForca[erros]} alt="" />
+      <button className="botaoescolher" onClick={() => escolherPalavra()}>Escolher palavra</button>
+      <div className="teclado">
+        <ul className="listaletras">
+          {palavra.map((letra, indice) => (
+            <li className={palavraerrada} key={indice}>
+              {letrasSelecionadas.includes(letra) || erros === 6 ? letra : "_"}
+            </li>)
+          )}
+        </ul>
       </div>
-    )
-  }
-
-  
-
- export default function SortearPalavra() {
-    const [disabled, setDisabled] = useState(false)
-  }
+    </div>
+  );
+}
--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,10 +1,69 @@
-export default function Letras(){
 
-    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  
-    return (
-      <div className="palavras"> 
-        {alfabeto.map( (postis) => <div disabled={true} className="palavra">{postis}</div>)}
-      </div>
-    )
+import React from "react";
+
+
+export default function Letras(props) {
+  const { letrasSelecionadas, setLetrasSelecionadas } = props;
+  const { disabled, setDisabled } = props;
+  let { palavra, disabledclasse, setDisabledclasse, letrasDisabled, setLetrasDisabled, erros, setErros, letraposta, setLetraposta } = props;
+  const { palavraerrada, setPalavraerrada } = props;
+
+
+  function clicarLetra(event, letras) {
+    let novoarray = [...letrasSelecionadas];
+    novoarray.push(letras);
+    setLetrasSelecionadas(novoarray);
+    if (!palavra.includes(letras)) {
+
+
+
+
+      setErros((erros) => erros + 1)
+      console.log(erros + 1)
+
+    }
+
+
+    if (erros + 1 === 6) {
+      setDisabled(true)
+      setPalavraerrada("errada")
+    }
+
+    let acertos = 0;
+
+    for (let i = 0; i < palavra.length; i++) {
+      if (letrasSelecionadas.includes(palavra[i])) {
+        acertos++;
+      }
+    }
+
+    if(acertos + 1 === palavra.length){
+      setPalavraerrada("certa")
+    }
+
+    console.log(acertos);
+
+
+    event.target.className = "palavra desabilitada";
+
+    event.target.disabled = true;
   }
+
+  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  return (
+    <div className="palavras">
+      {alfabeto.map((letra) => (
+        <button
+          key={letra}
+          disabled={disabled}
+          onClick={(event) => clicarLetra(event, letra)}
+          className={disabledclasse}
+
+        >
+          {letra}
+        </button>
+      ))}
+    </div>
+  );
+}
